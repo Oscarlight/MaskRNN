34f31e5c6462be5a0dc3b39577eed7b9cdd8bfbb
Oscarlight <oscarli2716@gmail.com>
2017-10-25 18:08:23 -0400

    decide to use dbreader for input

--- mask_rnn_lib.py
+++ mask_rnn_lib.py
@@ -11,19 +11,23 @@ log.setLevel(logging.DEBUG)
 
 class MaskRNN(object):
     def __init__(self, 
-        seq_lengths,
+        db_name,
         batch_size, 
         input_dim,
         output_dim,
         hidden_size,
+        iters,
         iters_to_report,
         ):
-    
-        self.seq_lengths = seq_lengths
+    	'''
+    	(T: seq size, N: batch size, D: input dim)
+    	seq_lengths: np.array of (N, 1) with each element is the seq length.
+    	'''
         self.batch_size = batch_size
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.hidden_size = hidden_size
+        self.iters = iters
         self.iters_to_report = iters_to_report
 
     def build_net(self):
@@ -87,12 +91,6 @@ class MaskRNN(object):
 
         workspace.RunNetOnce(self.model.param_init_net)
 
-        # As though we predict the same probability for each character
-        smooth_loss = -np.log(1.0 / self.D) * self.seq_length
-        last_n_iter = 0
-        last_n_loss = 0.0
-        num_iter = 0
-
         # Writing to output states which will be copied to input
         # states within the loop below
         workspace.FeedBlob(self.hidden_output, np.zeros(
@@ -100,71 +98,20 @@ class MaskRNN(object):
         ))
         workspace.CreateNet(self.prepare_state)
 
-        # We iterate over text in a loop many times. Each time we peak
-        # seq_length segment and feed it to LSTM as a sequence
-        last_time = datetime.now()
-        progress = 0
-        while True:
-            workspace.FeedBlob(
-                "seq_lengths", self.seq_lengths
-            )
-            workspace.RunNet(self.prepare_state.Name())
+        # Copy hidden_ouput to hidden_init
+        workspace.RunNet(self.prepare_state.Name())
 
-            input = np.zeros(
-                [self.seq_length, self.batch_size, self.D]
-            ).astype(np.float32)
-            target = np.zeros(
-                [self.seq_length * self.batch_size]
-            ).astype(np.int32)
+        # Add external inputs
+		workspace.FeedBlob("seq_lengths", self.seq_lengths)
+		# inputs: np.float32 [self.seq_length, self.batch_size, self.input_dim]
+		# target: np.float32 [self.seq_length, self.batch_size, self.output_dim]
+        workspace.FeedBlob('input_blob', inputs)
+        workspace.FeedBlob('target', target)
 
-            for e in range(self.batch_size):
-                for i in range(self.seq_length):
-                    pos = text_block_starts[e] + text_block_positions[e]
-                    input[i][e][self._idx_at_pos(pos)] = 1
-                    target[i * self.batch_size + e] =\
-                        self._idx_at_pos((pos + 1) % N)
-                    text_block_positions[e] = (
-                        text_block_positions[e] + 1) % text_block_sizes[e]
-                    progress += 1
+        CreateNetOnce(self.model.net)
+        workspace.RunNet(self.model.net.Name())
 
-            workspace.FeedBlob('input_blob', input)
-            workspace.FeedBlob('target', target)
 
-            CreateNetOnce(self.model.net)
-            workspace.RunNet(self.model.net.Name())
-
-            num_iter += 1
-            last_n_iter += 1
-
-            if num_iter % self.iters_to_report == 0:
-                new_time = datetime.now()
-                print("Characters Per Second: {}". format(
-                    int(progress / (new_time - last_time).total_seconds())
-                ))
-                print("Iterations Per Second: {}". format(
-                    int(self.iters_to_report /
-                        (new_time - last_time).total_seconds())
-                ))
-
-                last_time = new_time
-                progress = 0
-
-                print("{} Iteration {} {}".
-                      format('-' * 10, num_iter, '-' * 10))
-
-            loss = workspace.FetchBlob(self.loss) * self.seq_length
-            smooth_loss = 0.999 * smooth_loss + 0.001 * loss
-            last_n_loss += loss
-
-            if num_iter % self.iters_to_report == 0:
-                self.GenerateText(500, np.random.choice(self.vocab))
-
-                log.debug("Loss since last report: {}"
-                          .format(last_n_loss / last_n_iter))
-                log.debug("Smooth loss: {}".format(smooth_loss))
-
-                last_n_loss = 0.0
-                last_n_iter = 0
 
 def main():        # print(model)
     my_model = MaskRNN(
